66182cc7ff31276f77aedd3d8cb499fc10023f48
joningi98 <42949613+joningi98@users.noreply.github.com>
2020-10-27 16:13:04 +0000

    Entities

---
+++ Cryptocop.Software.API/Cryptocop.Software.API.Models/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace Cryptocop.Software.API.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
